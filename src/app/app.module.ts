3940a3798859dc9c6d41674fd21a6b93453221a8
Fernando Diaz M <yuloneitor@hotmail.com>
2018-10-01 08:50:35 -0500

    Modificacion 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { APP_ROUTES } from './app.routes';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { SidebarComponent } from './shared/sidebar/sidebar.component';
 import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
+import { IntroduccionComponent } from './pages/introduccion/introduccion.component';
+import { CapitulosComponent } from './pages/capitulos/capitulos.component';
+import { ActividadesComponent } from './pages/actividades/actividades.component';
+import { RetroalimentacionComponent } from './pages/retroalimentacion/retroalimentacion.component';
+import { AyudaComponent } from './pages/ayuda/ayuda.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     SidebarComponent,
-    BreadcrumbsComponent
+    BreadcrumbsComponent,
+    IntroduccionComponent,
+    CapitulosComponent,
+    ActividadesComponent,
+    RetroalimentacionComponent,
+    AyudaComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    APP_ROUTES
   ],
   providers: [],
   bootstrap: [AppComponent]
