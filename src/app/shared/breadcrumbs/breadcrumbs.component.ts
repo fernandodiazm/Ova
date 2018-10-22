f93848b8fc2e1b9e868b36ed114d8438a184ec50
Fernando Diaz M <yuloneitor@hotmail.com>
2018-10-22 00:05:07 -0500

    Se agg el breadcrumbs completo y se esta diseñando las actividades

--- src/app/shared/breadcrumbs/breadcrumbs.component.ts
+++ src/app/shared/breadcrumbs/breadcrumbs.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
+import { filter, map, distinctUntilChanged } from 'rxjs/operators';
+import { BreadCrumb } from './bread-crumb';
 
 @Component({
   selector: 'app-breadcrumbs',
@@ -6,10 +9,33 @@ import { Component, OnInit } from '@angular/core';
   styles: []
 })
 export class BreadcrumbsComponent implements OnInit {
+  breadcrumbs$ = this.router.events.pipe(
+    filter(event => event instanceof NavigationEnd),
+    distinctUntilChanged(),
+    map(event => this.buildBreadCrumb(this.activatedRoute.root))
+  );
 
-  constructor() { }
+  constructor(private activatedRoute: ActivatedRoute, private router: Router) {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  buildBreadCrumb(
+    route: ActivatedRoute,
+    url: string = '',
+    breadcrumbs: Array<BreadCrumb> = []
+  ): Array<BreadCrumb> {
+    const label = route.routeConfig
+      ? route.routeConfig.data['breadcrumb']
+      : 'Home';
+    const path = route.routeConfig ? route.routeConfig.path : '';
+    const nextUrl = `${url}${path}/`;
+    const breadcrumb = { label: label, url: nextUrl };
+    const newBreadcrumbs = [...breadcrumbs, breadcrumb];
+
+    if (route.firstChild) {
+      return this.buildBreadCrumb(route.firstChild, nextUrl, newBreadcrumbs);
+    }
+    return newBreadcrumbs;
   }
-
 }
+
